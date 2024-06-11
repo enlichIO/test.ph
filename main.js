54278f511fb9a9f41d6c8ddd3e0130436561226a
enlichio <17enrico@gmail.com>
2024-06-11 14:41:59 +0200

    hts

--- main.js
+++ main.js
@@ -2,16 +2,15 @@ const express = require('express')
 const path=require('path')
 
 const app = express();
-app.set('view engine','hbs');
-
+const port = process.env.PORT || 3000
 const public_Dir_path = path.join(__dirname,'./public')
 app.use(express.static(public_Dir_path))
 
 
 app.get('/', function(req, res){
-    res.render("index");
+    res.sendFile(path.join(__dirname,"./public/views/index.html"));
   })
   app.get('/product', function(req, res){
-    res.render("product")
+    res.sendFile(path.join(__dirname,"./public/views/product.html"))
   })
-  app.listen(3000)
+  app.listen(port)
